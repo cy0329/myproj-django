d78e7e9e4de0e7976e62970440928faa161e0350
Yeol Choi <chlduf153@gmail.com>
2022-01-07 10:40:46 +0900

    review_new 구현까지

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,5 +1,12 @@
+# from django import forms
 from rest_framework import serializers
 from shop.models import Review
+
+# 이건 장고만을 써서 구현하는 방법
+# class ReviewForm(forms.ModelForm):
+#     class Meta:
+#         model = Review
+#         fields = "__all__"
 
 
 class ReviewSerializer(serializers.ModelSerializer):
