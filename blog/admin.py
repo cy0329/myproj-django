fca93156ff1acd70bc75076073163b1973586bca
Yeol Choi <chlduf153@gmail.com>
2022-01-10 14:10:32 +0900

    admin에 Post모델 등록 - 장식자 사용

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from blog.models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+    list_display_links = "title"
 
