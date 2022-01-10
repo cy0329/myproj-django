07a910fd949b6b2fcefeecb0c27de0d6dd410dd7
Yeol Choi <chlduf153@gmail.com>
2022-01-10 14:31:13 +0900

    DRF사용 PostSerializer 생성, PostViewSet 생성, 기타 오류코드 수정

--- blog/admin.py
+++ blog/admin.py
@@ -6,5 +6,5 @@ from blog.models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["id", "title"]
-    list_display_links = "title"
+    list_display_links = ["title"]
 
