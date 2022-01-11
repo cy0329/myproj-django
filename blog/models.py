620294ccc8484c8c342c61a6adf565c4551e2c7f
Yeol Choi <chlduf153@gmail.com>
2022-01-11 18:24:59 +0900

    title max-length 100으로 변경

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,6 @@ class TimeStampedModel(models.Model):
 
 
 class Post(TimeStampedModel):
-    title = models.CharField(max_length=50, db_index=True)
+    title = models.CharField(max_length=100, db_index=True)
     content = models.TextField()
 
