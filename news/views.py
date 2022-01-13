3e09c949534f27b91ab5d40895555ad293b45adc
Yeol Choi <chlduf153@gmail.com>
2022-01-14 00:20:38 +0900

    복습 중 주석&변경점 등

--- news/views.py
+++ news/views.py
@@ -3,9 +3,9 @@ import json
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet  # <-- 아랫줄 하나하나가 다 포함되있는 부모
-from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView
 from news.models import Article
-from news.serializers import ArticleSerializer # ArticleAnonymousSerializer, ArticleGoldMembershipSerializer, ArticleAdminSerializer
+from news.serializers import ArticleSerializer # ArticleAnonymousSerializer, ArticleGoldMembershipSerializer,
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -13,29 +13,36 @@ class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
 
     # def get_serializer_class(self):
-    #     return ArticleSerializer
+    #     # return ArticleSerializer
+    #     # return ArticleAnonymousSerializer
+    #     # return ArticleGoldMembershipSerializer
+    #     return ArticleAdminSerializer
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-
-        query = self.request.query_params.get("query", "")
-        if query:
-            qs = qs.filter(title__icontains=query)
-
-        year = self.request.query_params.get("year", "")
-        if year:
-            qs = qs.filter(created_at__year=year)
-        return qs
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #
+    #     query = self.request.query_params.get("query", "")
+    #     if query:
+    #         qs = qs.filter(title__icontains=query)
+    #
+    #     year = self.request.query_params.get("year", "")
+    #     if year:
+    #         qs = qs.filter(created_at__year=year)
+    #     return qs
 
 # article_list = ListAPIView.as_view(
 #     queryset=Article.objects.all(),
 #     serializer_class=ArticleSerializer,
 # )
 
+# step 1
 # def article_list(request):
 #     qs = Article.objects.all()
+#
+#     # 2
 #     serializer = ArticleSerializer(qs, many=True)
 #     data = serializer.data
+#
 #     # data = [
 #     #     {
 #     #         "id": article.id,
