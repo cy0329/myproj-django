eb43e00f8161ef5a5817b98456e946fb960bac12
Yeol Choi <chlduf153@gmail.com>
2022-01-16 21:11:51 +0900

    검색 기능 추가 22.01.16.21:10

--- maple/views.py
+++ maple/views.py
@@ -7,3 +7,13 @@ from maple.serializers import CharacterSerializer
 class MapleViewSet(viewsets.ModelViewSet):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        query = self.request.query_params.get("query", "")
+        if query:
+            qs = qs.filter(job__icontains=query)
+
+        return qs
+
