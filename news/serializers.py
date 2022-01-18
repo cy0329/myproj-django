13e355b442d13962cbcbef611bdd0727d396df75
Yeol Choi <chlduf153@gmail.com>
2022-01-18 14:07:51 +0900

    article api 응답에서 author 필드를 추가

--- news/serializers.py
+++ news/serializers.py
@@ -1,12 +1,21 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 import re
 from news.models import Article
 
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "first_name", "last_name"]
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
     class Meta:
         model = Article
-        fields = ["id", "title", "content", "photo"]
+        fields = ["id", "title", "content", "photo", "author"]
 
     # def validate_title(self, title):
     #     if len(title) < 3:
