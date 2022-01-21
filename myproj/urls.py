8e44618d39d1a485d8ec93860f0b55dc8e4b8cab
Yeol Choi <chlduf153@gmail.com>
2022-01-21 16:16:52 +0900

    root 뷰를 구현

--- myproj/urls.py
+++ myproj/urls.py
@@ -6,8 +6,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='root.html'), name="root"),
     path("accounts/", include("accounts.urls")),
     path("admin/", admin.site.urls),
     path("shop/", include("shop.urls")),
