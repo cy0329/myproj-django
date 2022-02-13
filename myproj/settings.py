37f1e5b77537e39831cb4bbb1a262e670b019f42
Yeol Choi <chlduf153@gmail.com>
2022-02-13 23:58:42 +0900

    디버그 디폴트 세팅 변경

--- myproj/settings.py
+++ myproj/settings.py
@@ -19,7 +19,7 @@ if dot_env_path.exists():
 SECRET_KEY = env.str("SECRET_KEY", default="---- SECRET KEY ----")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DEBUG', default=True)
 
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
